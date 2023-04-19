c177d960ba4c99d6b409c423a9aec10e6efe1480
Omar Crosby <omar.crosby@gmail.com>
2023-04-19 11:32:11 -0400

    feat(sam): initializing sam

--- packages/aws-lambda-get-conference-details/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const awsLambdaGetConferenceDetails = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(awsLambdaGetConferenceDetails(), 'Hello from awsLambdaGetConferenceDetails');
-console.info('awsLambdaGetConferenceDetails tests passed');
